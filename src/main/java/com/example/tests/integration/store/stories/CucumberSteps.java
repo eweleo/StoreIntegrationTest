0273d1d8d0a37819f4d0fa4c88a731b1c206c863
eweleo <e.leonarczyk@gmail.com>
2022-04-22 17:33:20 +0200

    new test

--- src/main/java/com/example/tests/integration/store/stories/CucumberSteps.java
+++ src/main/java/com/example/tests/integration/store/stories/CucumberSteps.java
@@ -2,28 +2,35 @@ package com.example.tests.integration.store.stories;
 
 import com.example.tests.integration.store.CucumberTestContext;
 import com.example.tests.integration.store.stories.model.ChoiceProduct;
+import com.example.tests.integration.store.stories.model.Product;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.apache.http.HttpHeaders.TIMEOUT;
+
 
 public class CucumberSteps {
+
+
     private static final String REMOTE_URL = "http://localhost:8888";
     private CucumberTestContext cucumberTestContext = new CucumberTestContext();
 
     private ObjectMapper objectMapper = new ObjectMapper();
-
-
 
 
     @Given("remote service instance")
@@ -36,8 +43,11 @@ public class CucumberSteps {
     public void sendingRequestToService(String requestType, String path) {
         HttpMethod method = HttpMethod.valueOf(requestType);
         RestTemplate restTemplate = new RestTemplate();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        restTemplate.setRequestFactory(new
+                HttpComponentsClientHttpRequestFactory(httpClient));
         HttpEntity entity = HttpEntity.EMPTY;
-        String url =REMOTE_URL+"/"+path;
+        String url = REMOTE_URL + "/" + path;
 
 
         ResponseEntity<String> response = restTemplate.exchange(url, method, entity, String.class);
@@ -45,45 +55,60 @@ public class CucumberSteps {
     }
 
     @When("sending {string} request to {string} with {string}")
-    public void sendingRequestToServiceWithBody(String requestType, String path, String body){
+    public void sendingRequestToServiceWithBody(String requestType, String path, String body) throws Exception {
         HttpMethod method = HttpMethod.valueOf(requestType);
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(TIMEOUT);
-        requestFactory.setReadTimeout(TIMEOUT);
 
-        restTemplate.setRequestFactory(requestFactory);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        restTemplate.setRequestFactory(new
+                HttpComponentsClientHttpRequestFactory(httpClient));
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
 
-        HttpEntity entity = new HttpEntity<>(body,headers);
+        HttpEntity entity = new HttpEntity<>(body, headers);
 
+        if(method.equals(HttpMethod.PATCH)){
+            switch (path){
+                case  ("products"):
+                    Product product = objectMapper.readValue((String) cucumberTestContext.getResponse().getBody(),Product.class);
+                    String url = REMOTE_URL + "/" + path + "/" + product.getId();
+                    ResponseEntity<Product>response = restTemplate.exchange(url, method, entity, Product.class);
+                    cucumberTestContext.setResponse(response);
+                    break;
+
+            }
+
+        }else{
         String url = REMOTE_URL + "/" + path;
 
-        ResponseEntity<String> response = restTemplate.exchange(url,method,entity, String.class);
-        cucumberTestContext.setResponse(response);
+        ResponseEntity<String> response = restTemplate.exchange(url, method, entity, String.class);
+        cucumberTestContext.setResponse(response);}
     }
 
 
-
     @When("sending delete request to {string}")
-    public void sendingDeleteRequest( String path) throws Exception {
+    public void sendingDeleteRequest(String path) throws Exception {
         HttpMethod method = HttpMethod.DELETE;
         RestTemplate restTemplate = new RestTemplate();
 
         HttpEntity entity = HttpEntity.EMPTY;
-        String body = (String) cucumberTestContext.getResponse().getBody();
+
 
         switch (path) {
             case "choice_products":
 
                 ChoiceProduct choiceProduct = objectMapper.readValue((String) cucumberTestContext.getResponse().getBody(), ChoiceProduct.class);
-                String url = REMOTE_URL + "/" + path + "/" + choiceProduct.getId();
-                ResponseEntity<ChoiceProduct> response = restTemplate.exchange(url, method, entity, ChoiceProduct.class);
-                cucumberTestContext.setResponse(response);
+                String urlChoice = REMOTE_URL + "/" + path + "/" + choiceProduct.getId();
+                ResponseEntity<ChoiceProduct> choiceProductResponseEntity = restTemplate.exchange(urlChoice, method, entity, ChoiceProduct.class);
+                cucumberTestContext.setResponse(choiceProductResponseEntity);
                 break;
+            case "products":
+                Product product = (Product) cucumberTestContext.getResponse().getBody();
+                String urlProduct = REMOTE_URL + "/" + path + "/" +product.getId();
+                ResponseEntity<Product> productResponseEntity = restTemplate.exchange(urlProduct, method, entity,Product.class);
+                cucumberTestContext.setResponse(productResponseEntity);
         }
 
 
@@ -96,16 +121,16 @@ public class CucumberSteps {
     }
 
     @Then("response for post should be ok with body not null")
-    public void response_for_post_should_be_ok_with_body_not_null(){
+    public void response_for_post_should_be_ok_with_body_not_null() {
         Assert.assertEquals(HttpStatus.CREATED, cucumberTestContext.getResponse().getStatusCode());
         Assert.assertNotNull(cucumberTestContext.getResponse().getBody());
     }
 
-//    @Then("clean after when")
-//    public void cleanAfterWhen(){
-//        Assert.assertEquals(HttpEntity);
-//
-//    }
+    @Then("response for delete should be no content")
+    public void cleanAfterWhen(){
+        Assert.assertEquals(HttpStatus.NO_CONTENT, cucumberTestContext.getResponse().getStatusCode());
+
+    }
 
 
 }
--- src/main/resources/stories/feature/interface-tests.feature
+++ src/main/resources/stories/feature/interface-tests.feature
@@ -1,31 +1,62 @@
 Feature: abc
-  
-    Scenario Outline: GET to <string>
-      Given remote service instance
-      When sending "GET" request to <string>
-      Then response should be  with body not null
-      Examples:
-        | string            | 
-        | "products"        |
-        | "inventory"       |
-        | "users"           |
-        | "orders"          |
-        | "choice_products" |
 
-      Scenario Outline: PATCH to <string1>
-        Given remote service instance
-        When sending "PATCH" request to <string1> with <string2>
-        Then response should be  with body not null
-        Examples:
-          |string1              | string2                     |
-          | "products/4"        |"{\"name\": \"ca\"}"          |
-          | "inventory/2"       |"{\quantity\": 100}"         |
-          | "users/4"           |"{\"surname\": \"kowalski"\}"|
-          | "orders/9"          |""                           |
-          | "choice_products/8" |"{\"quantity\": 100          |
+  Scenario Outline: GET to <string>
+    Given remote service instance
+    When sending "GET" request to <string>
+    Then response should be  with body not null
+    Examples:
+      | string            |
+      | "products"        |
+      | "inventory"       |
+      | "users"           |
+      | "orders"          |
+      | "choice_products" |
 
-    Scenario: POST and Delete
-      Given remote service instance
-      When sending "POST" request to "choice_products" with "{\"userId\" : 2 , \"productId\" : 3 , \"quantity\" : 2}"
-      Then response for post should be ok with body not null
-      When sending delete request to "choice_products"
+
+  Scenario: Test products
+    Given remote service instance
+    When sending "POST" request to "products" with "{\"name\": \"product\"}"
+    Then response for post should be ok with body not null
+    When sending "PATCH" request to "products" with "{\"name\": \"new\"}"
+    Then response should be  with body not null
+    When sending delete request to "products"
+    Then response for delete should be no content
+
+
+  Scenario: Test patch2
+    Given remote service instance
+    When sending "PATCH" request to "inventory/2" with "{\"quantity\": 10}"
+    Then response should be  with body not null
+
+  Scenario: Test patch3
+    Given remote service instance
+    When sending "PATCH" request to "users/12" with "{\"name\": \"anna\", \"surname\": \"kowalski\"}"
+    Then response should be  with body not null
+
+  Scenario: Test patch3
+    Given remote service instance
+    When sending "PATCH" request to "orders/19"
+    Then response should be  with body not null
+
+
+
+
+  Scenario Outline: PATCH to <string1>
+    Given remote service instance
+    When sending "PATCH" request to <string1> with <string2>
+    Then response should be  with body not null
+    Examples:
+      | string1             | string2                       |
+      | "products/4"        | "{\"name\": \"ca\"}"          |
+      | "inventory/2"       | "{\"quantity\": 100}"         |
+      | "users/4"           | "{\"surname\": \"kowalski"\}" |
+      | "orders/9"          | ""                            |
+      | "choice_products/8" | "{\"quantity\": 100           |
+
+
+  Scenario: POST and Delete
+    Given remote service instance
+    When sending "POST" request to "choice_products" with "{\"userId\" : 12 , \"productId\" : 3 , \"quantity\" : 2}"
+    Then response for post should be ok with body not null
+    When sending delete request to "choice_products"
+    Then response for delete should be no content
